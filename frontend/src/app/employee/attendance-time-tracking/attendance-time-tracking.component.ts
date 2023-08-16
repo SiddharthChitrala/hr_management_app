e98e9689e35fd478762356e0c9ff752ecf22f52e
siddharth <siddharthchitrala4@gmail.com>
2023-08-16 23:14:12 +0530

    initial commit

--- frontend/src/app/employee/attendance-time-tracking/attendance-time-tracking.component.ts
+++ frontend/src/app/employee/attendance-time-tracking/attendance-time-tracking.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-attendance-time-tracking',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./attendance-time-tracking.component.css']
 })
 export class AttendanceTimeTrackingComponent {
+  attendanceForm: FormGroup;
+  goBack() {
+    window.location.reload();
+  }  
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.attendanceForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      date: ['', Validators.required],
+      time: ['', Validators.required]
+    });
+  }
 
+  submitAttendance() {
+    if (this.attendanceForm.valid) {
+      const attendanceData = this.attendanceForm.value;
+      
+      this.http.post('/api/attendance', attendanceData).subscribe(response => {
+        console.log('Attendance saved:', response);
+        this.attendanceForm.reset();
+      }, error => {
+        console.error('Error saving attendance:', error);
+      });
+    }
+  }
 }
