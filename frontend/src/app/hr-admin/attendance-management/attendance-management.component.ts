98edb33640563d9d4a01484c30a0927224264a5f
siddharth <siddharthchitrala4@gmail.com>
2023-08-17 14:27:09 +0530

    initial commit

--- frontend/src/app/hr-admin/attendance-management/attendance-management.component.ts
+++ frontend/src/app/hr-admin/attendance-management/attendance-management.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./attendance-management.component.css']
 })
 export class AttendanceManagementComponent {
-
+  goBack() {
+    location.reload();
+  }
+  Details: any[] = [];
+  constructor(private http: HttpClient) {
+    this.getAllDetails();
+  }
+  getAllDetails() {
+    this.http.get("http://localhost:9000/get").subscribe((resultData: any) => {
+      console.log(resultData);
+      this.Details = resultData.data;
+    })
+  }
 }
